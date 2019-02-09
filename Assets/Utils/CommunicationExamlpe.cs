59376462e0168030829fba899deb0ed18d08ee27
Andrés Felipe Cano <andresfe98@gmail.com>
2019-02-09 09:58:09 -0500

    First commit
    Initial files

---
+++ Assets/Utils/CommunicationExamlpe.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommunicationExamlpe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// // Update is called once per frame
+	// void Update () {
+		
+	// }
+
+	public void SendTrigger1(bool isSelected) {
+
+		
+		Communications.Send("UnityTrigger1", isSelected ? 1 : 0);
+
+	}
+
+	public void SendTrigger2(bool isSelected) {
+		Communications.Send("UnityTrigger2", isSelected ? 1 : 0);
+	}
+}
